8ef71cff6445d1188a740b45542f589093b27639
Arturas Salivonas <ecobra@gmail.com>
2019-05-01 15:09:53 +0100

    main color and hover are now editable via cms

--- front-page.php
+++ front-page.php
@@ -42,5 +42,98 @@
 	background-color: <?php the_field('global_background_color_'); ?>;
     background-image: url(<?php the_field('background_image_global'); ?>);
 }
+
+a{
+	color: <?php the_field('global_custom_color'); ?>;
+}
+a:hover{
+	color: <?php the_field('global_hover_custom_color'); ?>;
+}
+
+input[type=submit]:hover, input[type=submit]:focus, input[type=button]:focus, button:hover, button:focus, a.button:hover, a.button:focus {
+    background-color: <?php the_field('global_custom_color'); ?>;
+}
+.border-block-top-110 {
+    border-top: 3px solid <?php the_field('global_custom_color'); ?>;
+    border-left: 3px solid <?php the_field('global_custom_color'); ?>;
+}
+.header {
+    background-color: <?php the_field('global_custom_color'); ?>;
+}
+ul.social-links li a:hover{
+	background-color: <?php the_field('global_hover_custom_color'); ?>;
+}
+.menu-toggle i{
+	color: <?php the_field('global_custom_color'); ?>;
+}
+.site-main-menu li a:after{
+	background-color: <?php the_field('global_custom_color'); ?>;
+} 
+.section-description {
+    background-color: <?php the_field('global_custom_color'); ?>;
+}
+.home-page-block h1{
+	color: <?php the_field('global_custom_color'); ?>;
+}
+.info-list li .title:after{
+	 background-color: <?php the_field('global_custom_color'); ?>;
+}
+.event-date{
+	 background-color: <?php the_field('global_custom_color'); ?>;
+} 
+.event-date .event-icon{
+        border-top: 1px solid <?php the_field('global_custom_color'); ?>;
+        border-left: 1px solid <?php the_field('global_custom_color'); ?>;
+}
+.career-timeline .event-description {
+    color: <?php the_field('global_custom_color'); ?>;
+}
+.portfolio-grid  figure a div i{
+	color: <?php the_field('global_custom_color'); ?>;
+}
+
+.header.header-color-light .site-title {
+  	color: <?php the_field('global_custom_color'); ?>;
+}
+.header.header-color-light .site-main-menu li.active a:after {
+    background-color: <?php the_field('global_custom_color'); ?>;
+}
+ul.list ul:before {
+    border-left: 1px solid <?php the_field('global_custom_color'); ?>;
+}
+ul.list li a:hover,
+ul.list li a:hover+ul li a {
+    color: <?php the_field('global_custom_color'); ?>;
+}
+ul.list ul li:before {
+    border-top: 1px solid <?php the_field('global_custom_color'); ?>;
+}
+.visit--website_cta:hover{
+	color: <?php the_field('global_custom_color'); ?>;
+}
+
+.Buttonx {
+    background: <?php the_field('global_custom_color'); ?>;
+    box-shadow: 0 10px 60px -10px <?php the_field('global_custom_color'); ?>;
+}
+.Buttonx:hover{
+ 	 background: <?php the_field('global_hover_custom_color'); ?>;
+     box-shadow: 0 8px 65px -5px <?php the_field('global_hover_custom_color'); ?>;
+
+}
+.Buttonx:active{
+    background: <?php the_field('global_hover_custom_color'); ?>;
+    box-shadow: 0 8px 65px -5px <?php the_field('global_hover_custom_color'); ?>;
+
+}
+span[data-typer-targets] {
+    color: <?php the_field('global_custom_color'); ?>;
+}
+.navigation--cta{
+	 background: <?php the_field('global_custom_color'); ?>;
+}
+.navigation--cta:hover{
+	 background: <?php the_field('global_hover_custom_color'); ?>;
+}
 </style>
 <?php get_footer();?>
